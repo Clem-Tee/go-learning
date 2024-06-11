c9e6eba0bb5805da5eec1a1d5d2978c7003f7d33
Clem-Tee <clementogol@gmail.com>
2024-06-11 15:00:35 +0300

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"fmt"
 
 	"rsc.io/quote"
@@ -10,9 +11,24 @@ import (
 
 func main() {
 
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	fmt.Println(quote.Go())
+
+	// Set properties of the predefined Longer, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 
-	fmt.Println(quote.Go())
 }
